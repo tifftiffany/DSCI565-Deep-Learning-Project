06f3b48b1de842e6dffaee692013c406e20b90ac
tiffany wong <twong225@usc.edu>
2023-12-08 00:23:11 -0800

    processdata, util py upload

---
+++ utils.py
@@ -0,0 +1,143 @@
+from operator import itemgetter
+from itertools import combinations
+import time
+import os
+import math
+import numpy as np
+import networkx as nx
+import scipy.sparse as sp
+import scipy.io as sio
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import pandas as pd
+import h5py
+import pickle
+import torch
+from torch_geometric.utils import to_undirected
+from torch_geometric.data import InMemoryDataset
+from torch_geometric.data import Data
+from Processing_data import feature_matrix, edge_index, label
+
+
+# preprocessing
+def get_sparse_mat(a2b, a2idx, b2idx):
+    n = len(a2idx)
+    m = len(b2idx)
+    assoc = np.zeros((n, m))
+    for a, b_assoc in a2b.iteritems():
+        if a not in a2idx:
+            continue
+        for b in b_assoc:
+            if b not in b2idx:
+                continue
+            assoc[a2idx[a], b2idx[b]] = 1.
+    assoc = sp.coo_matrix(assoc)
+    return assoc
+
+
+def sparse_to_tuple(sparse_mx):
+    if not sp.isspmatrix_coo(sparse_mx):
+        sparse_mx = sparse_mx.tocoo()
+    coords = np.vstack((sparse_mx.row, sparse_mx.col)).transpose()
+    values = sparse_mx.data
+    shape = sparse_mx.shape
+    return coords, values, shape
+    
+def network_edge_threshold(network_adj, threshold):
+    edge_tmp, edge_value, shape_tmp = sparse_to_tuple(network_adj)
+    preserved_edge_index = np.where(edge_value>threshold)[0]
+    preserved_network = sp.csr_matrix(
+        (edge_value[preserved_edge_index], 
+        (edge_tmp[preserved_edge_index,0], edge_tmp[preserved_edge_index, 1])),
+        shape=shape_tmp)
+    return preserved_network
+    
+
+class DiseaseGeneDataset(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None):
+        super(DiseaseGeneDataset, self).__init__(root, transform,
+                                                 pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        return []
+
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
+
+    def download(self):
+        pass
+
+    def process(self):
+
+        data_list = []
+
+        data = Data(x=feature_matrix, edge_index=edge_index, y=label)
+        data_list.append(data)
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices),self.processed_paths[0])
+    
+def train_test_split_edges(data, val_ratio=0.1, test_ratio=0.1):
+    r"""Splits the edges of a :obj:`torch_geometric.data.Data` object
+    into positive and negative train/val/test edges, and adds attributes of
+    `train_pos_edge_index`, `train_neg_adj_mask`, `val_pos_edge_index`,
+    `val_neg_edge_index`, `test_pos_edge_index`, and `test_neg_edge_index`
+    to :attr:`data`.
+    Args:
+        data (Data): The data object.
+        val_ratio (float, optional): The ratio of positive validation
+            edges. (default: :obj:`0.05`)
+        test_ratio (float, optional): The ratio of positive test
+            edges. (default: :obj:`0.1`)
+    :rtype: :class:`torch_geometric.data.Data`
+    """
+
+    assert 'batch' not in data  # No batch-mode.
+
+    num_nodes = data.num_nodes
+    row, col = data.edge_index
+    data.edge_index = None
+
+    # Return upper triangular portion.
+    mask = row < col
+    row, col = row[mask], col[mask]
+
+    n_v = int(math.floor(val_ratio * row.size(0)))
+    n_t = int(math.floor(test_ratio * row.size(0)))
+
+    # Positive edges.
+    perm = torch.randperm(row.size(0))
+    row, col = row[perm], col[perm]
+
+    
+    r, c = row[:n_v], col[:n_v]
+    data.val_pos_edge_index = torch.stack([r, c], dim=0)
+    r, c = row[n_v:n_v + n_t], col[n_v:n_v + n_t]
+    data.test_pos_edge_index = torch.stack([r, c], dim=0)
+
+    r, c = row[n_v + n_t: n_v + n_t + 1500], col[n_v + n_t: n_v + n_t+1500]
+    data.train_pos_edge_index = torch.stack([r, c], dim=0)
+    data.train_pos_edge_index = to_undirected(data.train_pos_edge_index)
+
+    # Negative edges.
+    neg_adj_mask = torch.ones(num_nodes, num_nodes, dtype=torch.uint8)
+    neg_adj_mask = neg_adj_mask.triu(diagonal=1).to(torch.bool)
+    neg_adj_mask[row, col] = 0
+
+    neg_row, neg_col = neg_adj_mask.nonzero(as_tuple=False).t()
+    perm = torch.randperm(neg_row.size(0))[:n_v + n_t]
+    neg_row, neg_col = neg_row[perm], neg_col[perm]
+
+    neg_adj_mask[neg_row, neg_col] = 0
+    data.train_neg_adj_mask = neg_adj_mask
+
+    row, col = neg_row[:n_v], neg_col[:n_v]
+    data.val_neg_edge_index = torch.stack([row, col], dim=0)
+
+    row, col = neg_row[n_v:n_v + n_t], neg_col[n_v:n_v + n_t]
+    data.test_neg_edge_index = torch.stack([row, col], dim=0)
+
+    return data
